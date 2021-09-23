907d0043c5e34a992973efae3714a48a660fcb3e
Romain Lavidalie <83449831+RomainLavidalie@users.noreply.github.com>
2021-09-23 12:50:13 +0200

    started gameplay loop, character select

--- FightGame/Program.cs
+++ FightGame/Program.cs
@@ -3,8 +3,73 @@ using FightGame.Characters;
 
 namespace FightGame {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+            // Initializations
+
+            Random rng = new Random();
+            bool gameRunning = true;
+            Character player;
+            Character cpu;
+            int turn = 1;
             
+            // Character select
+            
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("| FIGHTGAME |");
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("");
+            Console.WriteLine("Choisissez votre classe : ");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Tank");
+            Console.WriteLine("2 - Healer");
+            Console.WriteLine("3 - Damager");
+            Console.WriteLine("");
+            
+            string playerChoice = Console.ReadLine();
+
+            switch (playerChoice)
+            {
+                case "1":
+                    //player = new Tank();
+                    break;
+                case "2":
+                    player = new Healer();
+                    break;
+                case "3":
+                    //player = new Damager();
+                    break;
+            }
+            
+            Console.WriteLine($"Vous avez choisi {player}.");
+            
+            // CPU character select
+            
+            int cpuChoice = rng.Next(1, 3);
+            switch (cpuChoice)
+            {
+                case 1:
+                    //cpu = new Tank();
+                    break;
+                case 2:
+                    cpu = new Healer();
+                    break;
+                case 3:
+                    //cpu = new Damager();
+                    break;
+            }
+            
+            // Gameplay loop
+
+            while (gameRunning == true)
+            {
+                Console.WriteLine("+--------+");
+                Console.WriteLine($"Tour {turn}");
+                Console.WriteLine("+--------+");
+                Console.WriteLine("");
+                
+            }
+
         }
     }
 }
